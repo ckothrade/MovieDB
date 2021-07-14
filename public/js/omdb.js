8c6d45fe8bc3426ff763c079a639b1547fa7c29b
ckothrade <cjkothrade@gmail.com>
2021-07-13 23:27:10 -0700

    Removed option to find a specific movie from the search page & also add gitignore file

--- public/js/omdb.js
+++ public/js/omdb.js
@@ -13,11 +13,11 @@ const mediaDiv = document.querySelector('#media');
 
 
 // Searches for a specific film from OMDB
-findBtn.onclick = function(event) {
-  event.preventDefault() // Stop page from reloading
-  const value = findElement.value;
+// findBtn.onclick = function(event) {
+//   event.preventDefault() // Stop page from reloading
+//   const value = findElement.value;
 
-  const newUrl = url + '&t=' + value;
+//   const newUrl = url + '&t=' + value;
 
   // fetch(newUrl)
   // .then((res) => res.json())
@@ -37,7 +37,7 @@ findBtn.onclick = function(event) {
 
 
   // console.log(newUrl);
-}
+// }
 
 
 // searchBtn.onclick = function(event) {
@@ -81,14 +81,15 @@ document.onclick = function(event) {
     const section = target.parentElement; // Section
     const content = section.nextElementSibling; // Content
     // content.innerHTML = showMedia(target.getAttribute('alt'));
-    content.innerHTML = '<p>CLICKED!</p>';
+    // content.innerHTML = '<p>CLICKED!</p>';
+    content.innerHTML = showMedia(jsonMovie);
   }
 }
 
-function showMedia(title) {
+function showMedia(movie) {
   // const content = document.querySelector('#content');
   // content.innerHTML = `<p>clicked</p>`;
-  return '<p>clicked!</p>';
+  return '<p>' + movie.Title + '</p>';
 }
   
 // Creates entire movie section
@@ -162,3 +163,10 @@ function createPosterList(data) {
 }
 
 const jsonData = JSON.parse('{"Search":[{"Title":"Jaws","Year":"1975","imdbID":"tt0073195","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BMmVmODY1MzEtYTMwZC00MzNhLWFkNDMtZjAwM2EwODUxZTA5XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"},{"Title":"Jaws 2","Year":"1978","imdbID":"tt0077766","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BN2U1MWE1NTMtYjQ2ZC00MTFmLWFmYjItODMyNGYxOTAyZmEzXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"},{"Title":"Jaws: The Revenge","Year":"1987","imdbID":"tt0093300","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BNGI1MTAxMWItOTE0OC00ZDhkLWE3Y2EtNjFiZmQ4NjQ1NGNkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"},{"Title":"Jaws 3","Year":"1983","imdbID":"tt0085750","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BM2QwYjhiMzQtN2RiYS00MzgxLThmNzItMmJlNmMyMmQyYTA3XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"},{"Title":"Jaws 19","Year":"2015","imdbID":"tt5033000","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BZjRmMzYzMTctZmE5MC00NWZjLTk5YmItYjQ1YTljMDdlYWYxXkEyXkFqcGdeQXVyNDcwNDE0Nzk@._V1_SX300.jpg"},{"Title":"Cruel Jaws","Year":"1995","imdbID":"tt0112747","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BYTgxN2I4ZjEtMjAyZS00MzM5LWE0MjAtZmQ4M2UyMjgwODcwXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"},{"Title":"Santa Jaws","Year":"2018","imdbID":"tt8305692","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BZjdmZjQ2ODktMzUwYy00MWRlLThiZWYtMzFkODBlN2I1OWZhXkEyXkFqcGdeQXVyMzQ5OTc2Mzk@._V1_SX300.jpg"},{"Title":"In the Jaws of Life","Year":"1984","imdbID":"tt0088312","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BMTIzNjU2ODUwOV5BMl5BanBnXkFtZTYwMzc2MDg4._V1_SX300.jpg"},{"Title":"Jaws of Satan","Year":"1981","imdbID":"tt0082580","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BMTUyOTY4Mzc0N15BMl5BanBnXkFtZTcwNTY2NjE2NA@@._V1_SX300.jpg"},{"Title":"The Making of Jaws","Year":"1995","imdbID":"tt0251821","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BZjkxNTBmZDgtOGUwZS00N2IwLThiMTYtYzljYmU5NzE1MGQyXkEyXkFqcGdeQXVyNTUyNzA5ODE@._V1_SX300.jpg"}],"totalResults":"103","Response":"True"}');
+
+
+const jsonMovie = JSON.parse('{"Title": "Jaws","Year": "1975",' +
+  '"Rated": "PG","Released": "20 Jun 1975","Runtime": "124 min",' +
+  '"Genre": "Adventure, Thriller","Director": "Steven Spielberg",' +
+  '"Poster": "https://m.media-amazon.com/images/M/MV5BMmVmODY1MzEtYTMwZC00MzNhLWFkNDMtZjAwM2EwODUxZTA5XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"}');
+
